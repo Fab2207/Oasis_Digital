2ae73d48e3b7822c24fb243d96eca12d8a52c681
user <user@hotmail.com>
2025-09-17 19:05:13 -0500

    feat: Inicializacion de proyecto de Sistema de Gestion Hotelera con Spring

---
+++ src/main/java/com/gestion/hotelera/HoteleraApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.hotelera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteleraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteleraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/hotelera/HoteleraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.hotelera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteleraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
