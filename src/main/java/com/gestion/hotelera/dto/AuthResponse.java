a50f0b5185dba7dc5051253e781a2146ec7a6cf9
Jean Pieer <jeanpieertapia@gmail.com>
2025-10-21 20:38:26 -0500

    Feat:Creamos la carpeta dto

---
+++ src/main/java/com/gestion/hotelera/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.gestion.hotelera.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class AuthResponse {
+    String token;
+}
---
+++ src/main/java/com/gestion/hotelera/dto/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.gestion.hotelera.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class LoginRequest {
+    String username;
+    String password;
+}
